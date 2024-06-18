99746a806f4ecf2710dc6097b019ff2d01828afe
Lennart Rosenvinge Mamsen <lennart.mamsen@luxion.com>
2024-06-18 14:35:56 +0200

    Switch back to yarn and updated the snapshot

--- .projenrc.js
+++ .projenrc.js
@@ -1,20 +1,18 @@
-const { AwsCdkConstructLibrary } = require('projen/lib/awscdk');
-const { NodePackageManager } = require('projen/lib/javascript');
+const { awscdk } = require('projen');
 
-const project = new AwsCdkConstructLibrary({
-  npmRegistryUrl: 'https://npmjs.com',
+const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '~5.4.0',
   docgen: false,
   author: 'KeyShot',
   authorAddress: 'open-source-maintainers@keyshot.com',
   defaultReleaseBranch: 'main',
-  cdkVersion: '2.135.0',
+  cdkVersion: '2.146.0',
   name: 'cdk-v2-stack-resource-rename-typescript',
   repositoryUrl: 'https://github.com/luxionkeyshot/cdk-v2-stack-resource-rename-NODEJS.git',
   license: 'Apache-2.0',
   constructsVersion: '10.3.0',
-  packageManager: NodePackageManager.NPM,
   jestOptions: {
+    updateSnapshot: 'NEVER',
     jestConfig: {
       testMatch: ['<rootDir>/**/*.test.ts'],
     },
