d58034b2f092b294814221da569febef0ba48821
Lennart Rosenvinge Mamsen <lennart.mamsen@luxion.com>
2024-06-19 06:19:31 +0200

    added typescript version

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,18 @@
 const { awscdk } = require('projen');
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  jsiiVersion: '~5.4.0',
+  prerelease: 'beta',
   docgen: false,
   author: 'KeyShot',
   authorAddress: 'open-source-maintainers@keyshot.com',
   defaultReleaseBranch: 'main',
-  cdkVersion: '2.146.0',
   name: 'cdk-v2-stack-resource-rename-typescript',
   repositoryUrl: 'https://github.com/luxionkeyshot/cdk-v2-stack-resource-rename-NODEJS.git',
   license: 'Apache-2.0',
+  jsiiVersion: '~5.4.0',
+  cdkVersion: '2.146.0',
   constructsVersion: '10.3.0',
+  typescriptVersion: '4.9.5',
   jestOptions: {
     updateSnapshot: 'NEVER',
     jestConfig: {
